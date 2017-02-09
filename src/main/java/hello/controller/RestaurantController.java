c23bf62a2148b45d076fc3972f60c651909dfa50
mr.6_moon <Ahmad.hawsa@stu.laroche.edu>
2017-02-09 14:56:01 -0500

    added 11 more restaurant in the RestaurantController...

--- src/main/java/hello/controller/RestaurantController.java
+++ src/main/java/hello/controller/RestaurantController.java
@@ -21,10 +21,9 @@ public class RestaurantController {
     }
 
     private void getRestaurantList() {
-        mRestaurantList.add(new Restaurant(40.570158, -80.023099, "BJ's Restaurant and Brewhouse"));
         mRestaurantList.add(new Restaurant(40.585317, -80.038578, "J.Clark's"));
         mRestaurantList.add(new Restaurant(40.570432, -80.022487, "Trader joes"));
-        mRestaurantList.add(new Restaurant(40.570123, -80.022864 , "BJ’s Restaurant & Brewhouse"));
+        mRestaurantList.add(new Restaurant(40.570142, -80.023067, "BJ’s Restaurant & Brewhouse"));
         mRestaurantList.add(new Restaurant(40.569697, -80.023064, "Bonefish Grill"));
         mRestaurantList.add(new Restaurant(40.571283, -80.022857, "Longhorn Steak House"));
         mRestaurantList.add(new Restaurant(40.570130, -80.021570, "Hello Bistro"));
@@ -44,7 +43,17 @@ public class RestaurantController {
         mRestaurantList.add(new Restaurant(40.552242, -80.018919, "The Original Pancake House"));
         mRestaurantList.add(new Restaurant(40.549701, -80.019280, "Mad Mex"));
         mRestaurantList.add(new Restaurant(40.544793, -80.016047, "Eat’n Park"));
-
+        mRestaurantList.add(new Restaurant(40.562565, -80.024733, "TGI Fridays"));
+        mRestaurantList.add(new Restaurant(40.561476, -80.024622, "Empire Palace"));
+        mRestaurantList.add(new Restaurant(40.567303, -80.000116, "Blue North Restaurant"));
+        mRestaurantList.add(new Restaurant(40.566170, -79.998191, "Bellisario's"));
+        mRestaurantList.add(new Restaurant(40.547513, -80.018515, "Taj Mahal"));
+        mRestaurantList.add(new Restaurant(40.572018, -80.037315, "Bistro 9101"));
+        mRestaurantList.add(new Restaurant(40.542045, -80.010538, "The Cheesecake Factory"));
+        mRestaurantList.add(new Restaurant(40.546672, -80.019432, "Sesame Inn Chinese Restaurant"));
+        mRestaurantList.add(new Restaurant(40.605293, -80.006013, "OTB Bicycle Cafe"));
+        mRestaurantList.add(new Restaurant(40.544898, -79.972638, "Le Thai Cafe"));
+        mRestaurantList.add(new Restaurant(40.517460, -80.004968, "Red Lobster"));
 
     }
 
