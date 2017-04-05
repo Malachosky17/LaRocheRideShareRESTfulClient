b56849733bd39dad56c718d4a9178de431f7b81c
Jumanie <jumanie.lubrin@stu.laroche.edu>
2017-04-05 19:21:44 -0400

    Adding more utilities to the utilities controller

--- src/main/java/hello/controller/UtilitiesController.java
+++ src/main/java/hello/controller/UtilitiesController.java
@@ -33,5 +33,15 @@ public class UtilitiesController {
         mUtilityList.add(new Utility(40.572271, -80.036587, "Sunoco Gas 2"));
         mUtilityList.add(new Utility(90.0, 51.06, "Passavant Hospital"));
         mUtilityList.add(new Utility(40.545266, -80.016909, "GetGo Gas"));
+        mUtilityList.add(new Utility(40.561714, -80.039545, "Sheetz"));
+        mUtilityList.add(new Utility(40.554138, -80.019387, "PNC"));
+        mUtilityList.add(new Utility(40.556393, -80.019204, "Dollar Bank"));
+        mUtilityList.add(new Utility(40.555741, -80.020009, "Huntington"));
+        mUtilityList.add(new Utility(40.579444, -80.027101, "Urgent Care"));
+        mUtilityList.add(new Utility(40.541754, -80.014055, "MedExpress"));
+        mUtilityList.add(new Utility(40.540468, -80.011580, "Mr. Tire"));
+        mUtilityList.add(new Utility(40.521309, -80.004593, "Autozone"));
+
+
     }
 }
