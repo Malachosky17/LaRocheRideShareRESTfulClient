e0e0486572ee6e90fcc50e8277ecc5df1acd98a6
Joseph Malachosky <Joseph.Malachosky@stu.laroche.edu>
2017-04-05 18:48:24 -0400

    Making changes to the Restaurant class and RestaurantController to enable a geolocation radius attribute.

--- src/main/java/hello/controller/EventController.java
+++
@@ -1,8 +0,0 @@
-package hello.controller;
-
-/**
- * Created by wargold419 on 3/21/17.
- */
-public class EventController {
-
-}
--- src/main/java/hello/model/Event.java
+++
@@ -1,8 +0,0 @@
-package hello.model;
-
-/**
- * Created by wargold419 on 3/21/17.
- */
-public class Event {
-
-}
